ef2e9984fed7c77d43e7af5534c6cb8ce4125620
Catherine Loesch <catherine.loesch@protonmail.com>
2021-10-03 01:13:33 +0100

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(arr)
+  arr.map.with_index do |element, index|
+    puts "#{index + 1}. #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  longer_than_four = false
+  arr.each do |element|
+    if element.size > 4
+      longer_than_four = true
+    end
+  end
+  longer_than_four
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  arr.find do |ingredient|
+    cheese_types.include?(ingredient)
+  end
 end
